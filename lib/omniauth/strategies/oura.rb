2a4bcd95db4bebb68641980cc01507cb0bfa899a
Bethany Soule <bsoule@gmail.com>
2024-07-06 16:22:06 -0700

    oura strategy & raw info stuff

--- lib/omniauth/strategies/oura.rb
+++ lib/omniauth/strategies/oura.rb
@@ -12,10 +12,33 @@ module OmniAuth
         :token_url => 'https://api.ouraring.com/oauth/token',
         :response_type => 'code'
       }
+      def authorize_params
+        super.tap do |params|
+          log(:debug, params[:scope])
+        end
+      end 
+      def setup_phase
+        super
+      end
+      def request_phase
+        super
+      end
+      def callback_phase
+        super
+      end
       # Bugfix for regression introduced after omniauth-oauth2 v1.3.1
       # details: https://github.com/intridea/omniauth-oauth2/issues/8
       def callback_url
         options[:callback_url] || (full_host + script_name + callback_path)
+      end
+      info do
+        { email: raw_info['email'] }
+      end
+      extra do 
+        { raw_info: raw_info }
+      end
+      def raw_info 
+        @raw_info ||= MultiJson.load(access_token.get("https://api.ouraring.com/v2/usercollection/personal_info").body) 
       end
     end
   end
